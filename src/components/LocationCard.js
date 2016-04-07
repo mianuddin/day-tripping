afc6d412af11c1268da59c7294599942de228a8a
Mian Uddin <mianuddin@gmail.com>
2016-04-07 16:31:51 -0700

    Changed stateless functions to functional components.

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,27 +1,17 @@
 import React from 'react';
 
-class LocationCard extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="row">
-        <div className="col s12 m6">
-          <div className="card blue-grey darken-1">
-            <div className="card-content white-text">
-              <span className="card-title">{this.props.name}</span>
-              <p>{this.props.address}</p>
-            </div>
-          </div>
+const LocationCard = props => (
+  <div className="row">
+    <div className="col s12 m6">
+      <div className="card blue-grey darken-1">
+        <div className="card-content white-text">
+          <span className="card-title">{props.name}</span>
+          <p>{props.address}</p>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 LocationCard.propTypes = {
   name: React.PropTypes.string,
--- src/components/LocationContainer.js
+++ src/components/LocationContainer.js
@@ -2,24 +2,14 @@ import React from 'react';
 
 import LocationCard from './LocationCard';
 
-class LocationContainer extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>Locations</h3>
-        {this.props.locations.map((location, index) => (
-          <LocationCard name={location.name} address={location.address} key={index} />
-        ))}
-      </div>
-    );
-  }
-}
+const LocationContainer = props => (
+  <div>
+    <h3>Locations</h3>
+    {props.locations.map((location, index) => (
+      <LocationCard name={location.name} address={location.address} key={index} />
+    ))}
+  </div>
+);
 
 LocationContainer.propTypes = {
   locations: React.PropTypes.array,
