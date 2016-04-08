853fcaa80909257f4f12a035bc3b5defde0b7e2e
Mian Uddin <mianuddin@gmail.com>
2016-04-08 10:03:45 -0700

    Added material-ui.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import { createHashHistory } from 'history';
 
 import routes from './routes';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 ReactDOM.render(
