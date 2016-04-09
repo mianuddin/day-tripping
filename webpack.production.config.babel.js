ab5d64c21807ee7eb42f18b896b7c9be8b538d5c
Mian Uddin <mianuddin@gmail.com>
2016-04-08 20:55:08 -0700

    Made separate production config for webpack.

---
+++ webpack.production.config.babel.js
@@ -0,0 +1,33 @@
+import webpack from 'webpack';
+
+export default {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js',
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'react-hot!babel',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  output: {
+    path: 'dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
