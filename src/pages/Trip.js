580b756ac37741f974e66fcb31be29b0c3c072a3
Mian Uddin <mianuddin@gmail.com>
2016-04-10 18:42:59 -0700

    Fixed line length issues

--- src/pages/Trip.js
+++ src/pages/Trip.js
@@ -20,23 +20,65 @@ class Trip extends React.Component {
   }
 
   componentWillMount() {
-    LocationStore.on('change_location', this.getLocations);
-    LocationStore.on('fetching_geolocation', this.setSnackbar.bind(this, 'Requesting geolocation...'));
-    LocationStore.on('recieved_geolocation', this.setSnackbar.bind(this, 'Recieved geolocation.', 1500));
-    LocationStore.on('error_geolocation', this.setSnackbar.bind(this, 'Could not detect your location!'));
-    LocationStore.on('fetching_latlng', this.setSnackbar.bind(this, 'Adding location...'));
-    LocationStore.on('added_location', this.setSnackbar.bind(this, 'Added location.', 1500));
-    LocationStore.on('error_latlng', this.setSnackbar.bind(this, 'Could not find the coordinates of your location!'));
+    LocationStore.on(
+      'change_location',
+      this.getLocations
+    );
+    LocationStore.on(
+      'fetching_geolocation',
+      this.setSnackbar.bind(this, 'Requesting geolocation...')
+    );
+    LocationStore.on(
+      'recieved_geolocation',
+      this.setSnackbar.bind(this, 'Recieved geolocation.', 1500)
+    );
+    LocationStore.on(
+      'error_geolocation',
+      this.setSnackbar.bind(this, 'Could not detect your location!')
+    );
+    LocationStore.on(
+      'fetching_latlng',
+      this.setSnackbar.bind(this, 'Adding location...')
+    );
+    LocationStore.on(
+      'added_location',
+      this.setSnackbar.bind(this, 'Added location.', 1500)
+    );
+    LocationStore.on(
+      'error_latlng',
+      this.setSnackbar.bind(this, 'Could not find the coordinates of your location!')
+    );
   }
 
   componentWillUnmount() {
-    LocationStore.removeListener('change_location', this.getLocations);
-    LocationStore.removeListener('fetching_geolocation', this.setSnackbar.bind(this, 'Requesting geolocation...'));
-    LocationStore.removeListener('recieved_geolocation', this.setSnackbar.bind(this, 'Recieved geolocation.', 1500));
-    LocationStore.removeListener('error_geolocation', this.setSnackbar.bind(this, 'Could not detect your location!'));
-    LocationStore.removeListener('fetching_latlng', this.setSnackbar.bind(this, 'Adding location...'));
-    LocationStore.removeListener('added_location', this.setSnackbar.bind(this, 'Added location.', 1500));
-    LocationStore.removeListener('error_latlng', this.setSnackbar.bind(this, 'Could not find the coordinates of your location!'));
+    LocationStore.removeListener(
+      'change_location',
+      this.getLocations
+    );
+    LocationStore.removeListener(
+      'fetching_geolocation',
+      this.setSnackbar.bind(this, 'Requesting geolocation...')
+    );
+    LocationStore.removeListener(
+      'recieved_geolocation',
+      this.setSnackbar.bind(this, 'Recieved geolocation.', 1500)
+    );
+    LocationStore.removeListener(
+      'error_geolocation',
+      this.setSnackbar.bind(this, 'Could not detect your location!')
+    );
+    LocationStore.removeListener(
+      'fetching_latlng',
+      this.setSnackbar.bind(this, 'Adding location...')
+    );
+    LocationStore.removeListener(
+      'added_location',
+      this.setSnackbar.bind(this, 'Added location.', 1500)
+    );
+    LocationStore.removeListener(
+      'error_latlng',
+      this.setSnackbar.bind(this, 'Could not find the coordinates of your location!')
+    );
   }
 
   getLocations() {
