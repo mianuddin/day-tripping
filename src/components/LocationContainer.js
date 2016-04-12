9a13848a36ab9a80a3d70b0b754eaa874f2eac56
Mian Uddin <mianuddin@gmail.com>
2016-04-12 09:52:17 -0700

    Misc. updates.

--- src/components/LocationContainer.js
+++ src/components/LocationContainer.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import LocationCard from './LocationCard';
 
-const LocationContainer = props => (
+const LocationContainer = (props) => (
   <div>
     {props.locations.map((location, index) => (
       <LocationCard location={location} key={index} onRemove={props.onRemove} />
     ))}
-    {!props.locations.length ? <p>You have no locations. Try adding one!</p> : null}
+    {
+      ! props.locations.length
+      ? <div className="emptyState"><p>You have no locations. Try adding one!</p></div>
+      : null
+    }
   </div>
 );
 
