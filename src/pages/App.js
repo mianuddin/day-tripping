af8b7abb5bd0827d0e01cd765a66295e5a738588
Mian Uddin <mianuddin@gmail.com>
2016-04-17 13:24:51 -0700

    Added MUI Theme, moved non scss/css styling to 'other' subdirectory

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import AppBar from 'material-ui/lib/app-bar';
 
-const App = props => (
-  <div>
-    <AppBar
-      title="Day Tripping"
-      showMenuIconButton={false}
-    />
-    <div className="container">
-      {props.children}
-    </div>
-  </div>
-);
+import ThemeManager from 'material-ui/lib/styles/theme-manager';
+import MyRawTheme from '../styles/other/MUITealTheme';
+import '../styles/partials/_base.scss';
+// TODO: Replace with partials/ add modules.
+import '../styles/stylesheet.scss';
+
+class App extends React.Component {
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getMuiTheme(MyRawTheme),
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <AppBar
+          title="Day Tripping"
+          showMenuIconButton={false}
+        />
+        <div className="container">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
 
 App.propTypes = {
   children: React.PropTypes.node,
+};
+
+App.childContextTypes = {
+  muiTheme: React.PropTypes.object,
 };
 
 export default App;
