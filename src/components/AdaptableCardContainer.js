d290081c988fae56d350aed19d73079547374740
Mian Uddin <mianuddin@gmail.com>
2016-05-03 23:08:11 -0700

    Changed props of AdaptableCardContainer.

--- src/components/AdaptableCardContainer.js
+++ src/components/AdaptableCardContainer.js
@@ -9,14 +9,13 @@ const AdaptableCardContainer = props => (
   <div id="AdaptableCardContainer">
     <h2>Locations</h2>
     <LocationContainer locations={props.locations} onRemove={props.onRemove} />
-    <Form onUserInput={props.onUserInput} />
+    <Form />
   </div>
 );
 
 AdaptableCardContainer.propTypes = {
   locations: React.PropTypes.array,
   onRemove: React.PropTypes.func,
-  onUserInput: React.PropTypes.func,
 };
 
 export default AdaptableCardContainer;
