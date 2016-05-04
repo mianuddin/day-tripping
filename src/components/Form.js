1fc947dd8d9429552547df724aa507f292cfe8d5
Mian Uddin <mianuddin@gmail.com>
2016-05-04 09:28:04 -0700

    Added validation to form and form clears on submit.

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,43 +4,54 @@ import Formsy from 'formsy-react';
 import FormsyText from 'formsy-material-ui/lib/FormsyText';
 import RaisedButton from 'material-ui/lib/raised-button';
 
-const Form = props => {
-  function submitForm(data) {
-    props.handleSubmit(data.locationName, data.locationAddress);
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.submitForm = this.submitForm.bind(this);
+  }
+  submitForm(data) {
+    this.props.handleSubmit(data.locationName, data.locationAddress);
+    this.refs.form.reset();
   }
 
-  return (
-    <Formsy.Form
-      onValidSubmit={submitForm}
-    >
-      <div className="row">
-        <div className="col-xs-12">
-          <FormsyText
-            required
-            name="locationName"
-            hintText="SF City Hall"
-            floatingLabelText="Location Name"
+  render() {
+    return (
+      <Formsy.Form
+        ref="form"
+        onValidSubmit={this.submitForm}
+      >
+        <div className="row">
+          <div className="col-xs-12">
+            <FormsyText
+              required
+              name="locationName"
+              hintText="SF City Hall"
+              floatingLabelText="Location Name"
+              validations="isExisty"
+            />
+          </div>
+          <div className="col-xs-12">
+            <FormsyText
+              required
+              name="locationAddress"
+              hintText="San Francisco, CA 94102"
+              floatingLabelText="Location Address"
+              validations="isExisty"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <RaisedButton
+            type="submit"
+            label="Submit"
+            primary
           />
         </div>
-        <div className="col-xs-12">
-          <FormsyText
-            required
-            name="locationAddress"
-            hintText="San Francisco, CA 94102"
-            floatingLabelText="Location Address"
-          />
-        </div>
-      </div>
-      <div className="row">
-        <RaisedButton
-          type="submit"
-          label="Submit"
-          primary
-        />
-      </div>
-    </Formsy.Form>
-  );
-};
+      </Formsy.Form>
+    );
+  }
+}
 
 Form.propTypes = {
   handleSubmit: React.PropTypes.func,
