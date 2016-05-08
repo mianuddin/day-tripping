4b20ee766e93979a0f53b5aab9b9a28e9d87c0c8
Mian Uddin <mianuddin@gmail.com>
2016-05-07 20:04:21 -0700

    Fixed JSDOM.

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,17 +2,24 @@ import jsdom from 'jsdom';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
 
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const PhantomJS = 'Mozilla/5.0 (Macintosh; Intel Mac OS X) AppleWebKit/538.1 (KHTML, like Gecko) PhantomJS/2.0.0 Safari/538.1';
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>', {
+  userAgent: PhantomJS,
+});
 const win = doc.defaultView;
 
 global.document = doc;
 global.window = win;
-global.navigator = win.navigator;
 
-Object.keys(window).forEach((key) => {
-  if (!(key in global)) {
+function propagateToGlobal(window) {
+  for (const key in window) {
+    if (!window.hasOwnProperty(key)) continue;
+    if (key in global) continue;
+
     global[key] = window[key];
   }
-});
+}
+
+propagateToGlobal(win);
 
 chai.use(chaiImmutable);
