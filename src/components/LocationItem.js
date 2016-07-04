96ef52ceb430092cf3afe8ab2a7ad6abbbb3122e
Mian Uddin <mianuddin@gmail.com>
2016-07-04 00:13:32 -0700

    Replaced location cards with location list.

---
+++ src/components/LocationItem.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import ListItem from 'material-ui/lib/lists/list-item';
+import Colors from 'material-ui/lib/styles/colors';
+import IconButton from 'material-ui/lib/icon-button';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+
+const LocationItem = props => {
+  const iconButtonElement = (
+    <IconButton
+      touch
+      tooltip="more"
+      tooltipPosition="bottom-left"
+    >
+      <MoreVertIcon color={Colors.grey400} />
+    </IconButton>
+  );
+
+  const rightIconMenu = (
+    <IconMenu iconButtonElement={iconButtonElement}>
+      <MenuItem
+        onTouchTap={props.onRemove.bind(this, props.location.id)} // eslint-disable-line
+      >
+        Remove
+      </MenuItem>
+    </IconMenu>
+  );
+
+  return (
+    <ListItem
+      primaryText={props.location.name}
+      rightIconButton={rightIconMenu}
+      secondaryText={props.location.address}
+    />
+  );
+};
+
+LocationItem.propTypes = {
+  location: React.PropTypes.object,
+  onRemove: React.PropTypes.func,
+};
+
+export default LocationItem;
--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,19 +1,41 @@
 import React from 'react';
 
-import LocationCard from './LocationCard';
+import List from 'material-ui/lib/lists/list';
+import Icon from 'material-ui/lib/svg-icons/maps/near-me';
+import LocationItem from './LocationItem';
 
-const LocationList = (props) => (
-  <div>
-    {props.locations.map((location, index) => (
-      <LocationCard location={location} key={index} onRemove={props.onRemove} />
-    ))}
-    {
-      ! props.locations.length
-      ? <div className="LocationList__empty-state"><p>You have no locations. Try adding one!</p></div>
-      : null
-    }
-  </div>
-);
+const LocationList = (props) => {
+  const ListJSX = (
+    <List>
+      {props.locations.map((location, index) => (
+        <LocationItem location={location} key={index} onRemove={props.onRemove} />
+      ))}
+    </List>
+  );
+
+  const EmptyStateJSX = (
+    <div className="LocationList--empty">
+      <Icon
+        className="LocationList__Icon"
+        style={{
+          height: 'auto',
+          width: '100px',
+        }}
+      />
+      <p>You have no locations. <br />Try adding one!</p>
+    </div>
+  );
+
+  return (
+    <div className="LocationList">
+      {
+        ! props.locations.length
+        ? EmptyStateJSX
+        : ListJSX
+      }
+    </div>
+  );
+};
 
 LocationList.propTypes = {
   locations: React.PropTypes.array,
