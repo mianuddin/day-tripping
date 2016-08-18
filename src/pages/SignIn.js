30f327d2036332f3a2300d8ec8ae6878fbf299ad
Mian Uddin <mianuddin@gmail.com>
2016-08-18 10:23:59 -0700

    made sign in look better

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import AppBar from 'material-ui/lib/app-bar';
+import RaisedButton from 'material-ui/lib/raised-button';
+import { Link } from 'react-router';
 
 import '../styles/scss/_modules/_SignIn.scss';
+
+const style = {
+  margin: 12,
+};
 
 class SignIn extends React.Component {
   render() {
@@ -19,13 +25,33 @@ class SignIn extends React.Component {
             fontFamily: 'Poppins, sans-serif',
           }}
         />
-        <h2>Sign in</h2>
+        <div className="container">
+          <div className="row center-xs">
+            <h2>Sign in</h2>
+          </div>
 
-        <button onClick={this.props.attemptLogin}>Log in</button>
-        <button onClick={this.props.logoutUser}>Log out</button>
+          <div className="row center-xs">
+            <div className="col-xs">
+              <RaisedButton label="Get Started" style={style} onClick={this.props.attemptLogin} />
+            </div>
+          </div>
 
-        <h3>Debug</h3>
-        <p>{JSON.stringify(this.props.auth)}</p>
+          <div className="row center-xs">
+            <div className="col-xs">
+              <Link to="/app">
+                <RaisedButton label="App" primary style={style} />
+              </Link>
+              <RaisedButton label="Log Out" secondary style={style} onClick={this.props.logoutUser} />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-xs">
+              <h3>Debug</h3>
+              <p>{JSON.stringify(this.props.auth)}</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
