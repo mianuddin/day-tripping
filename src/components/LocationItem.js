287a148d3f43fb865ee1fc8c48012446c7eda939
Mian Uddin <mianuddin@gmail.com>
2016-08-21 19:14:24 -0700

    added more menu items:

--- src/components/LocationItem.js
+++ src/components/LocationItem.js
@@ -6,6 +6,10 @@ import IconButton from 'material-ui/lib/icon-button';
 import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
 import IconMenu from 'material-ui/lib/menus/icon-menu';
 import MenuItem from 'material-ui/lib/menus/menu-item';
+import Divider from 'material-ui/lib/divider';
+import Delete from 'material-ui/lib/svg-icons/action/delete';
+import Directions from 'material-ui/lib/svg-icons/maps/directions';
+import Edit from 'material-ui/lib/svg-icons/editor/mode-edit';
 
 const LocationItem = props => {
   const iconButtonElement = (
@@ -21,10 +25,22 @@ const LocationItem = props => {
   const rightIconMenu = (
     <IconMenu iconButtonElement={iconButtonElement}>
       <MenuItem
+        primaryText="Navigate"
+        leftIcon={<Directions />}
+        linkButton
+        href={`https://www.google.com/maps/dir/Current+Location/${props.location.lat},${props.location.lng}`}
+      />
+      <Divider />
+      <MenuItem
+        primaryText="Edit"
+        leftIcon={<Edit />}
+        disabled
+      />
+      <MenuItem
+        primaryText="Remove"
+        leftIcon={<Delete />}
         onTouchTap={props.onRemove.bind(this, props.location.id)} // eslint-disable-line
-      >
-        Remove
-      </MenuItem>
+      />
     </IconMenu>
   );
 
