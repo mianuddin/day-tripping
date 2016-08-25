34c61e95b1c78a998498503c1114f635bb709a9c
Mian Uddin <mianuddin@gmail.com>
2016-08-24 20:24:35 -0700

    added progress indicator to landing and restructured page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,60 +1,80 @@
 import React from 'react';
 import AppBar from 'material-ui/lib/app-bar';
 import RaisedButton from 'material-ui/lib/raised-button';
+import LinearProgress from 'material-ui/lib/linear-progress';
 
 import '../styles/scss/_modules/_Landing.scss';
 
+const progressComponent = (
+  <LinearProgress
+    mode="indeterminate"
+    color="#fff"
+  />
+);
+
 const Landing = (props) => (
   <div>
-    <header
-      className="Hero"
-      style={{
-        backgroundImage: 'url(img/map.png)',
-      }}
-    >
-      <AppBar
-        title="Day Tripping"
-        showMenuIconButton={false}
+    <div className="Hero">
+      <div
+        className="Hero__Main"
         style={{
-          background: 'none',
-          boxShadow: 'none',
+          backgroundImage: 'url(img/map.png)',
         }}
-        titleStyle={{
-          color: '#FFF',
-          fontFamily: 'Poppins, sans-serif',
-        }}
-      />
-      <div className="container Hero--height">
-        <div className="row center-xs middle-xs Hero--height">
-          <div className="col-xs-12">
-            <span className="Hero__Headline">Explore your world</span>
+      >
+        { props.auth.currently === 'AWAITING_AUTH_RESPONSE' ? progressComponent : null }
+
+        <AppBar
+          title="Day Tripping"
+          showMenuIconButton={false}
+          style={{
+            background: 'none',
+            boxShadow: 'none',
+          }}
+          titleStyle={{
+            color: '#FFF',
+            fontFamily: 'Poppins, sans-serif',
+          }}
+        />
+
+        <div className="container">
+          <div className="row center-xs middle-xs">
+            <div className="col-xs-12">
+              <span className="Hero__Headline">Explore your world</span>
+            </div>
           </div>
         </div>
       </div>
-    </header>
-    <div className="container Remainder">
-      <div className="row center-xs middle-xs Remainder__content">
-        <div className="col-xs-10 col-sm-12">
-          <span className="Remainder__tagline">
-            Easily keep track of the places you want to be, for free.
-          </span><br />
-          <RaisedButton
-            label="Get Started"
-            style={{
-              margin: '1rem',
-            }}
-            onClick={props.attemptLogin}
-          />
-        </div>
-      </div>
-      <div className="row center-xs">
-        <div className="col-xs">
-          <span className="Remainder__footer">
-            Designed and Developed with <a href="https://github.com/mianuddin/day-tripping">❤</a> by <a href="http://mianudd.in/">Mian Uddin</a>
-          </span>
+
+      <div className="container Hero__Content">
+        <div className="row center-xs middle-xs">
+          <div className="col-xs-10 col-sm-12">
+            <span className="Hero__tagline">
+              Easily keep track of the places you want to be, for free.
+            </span><br />
+
+            <RaisedButton
+              label="Get Started"
+              style={{
+                margin: '1rem',
+              }}
+              onClick={props.attemptLogin}
+            />
+          </div>
         </div>
       </div>
     </div>
+
+    <footer className="Footer">
+      <div className="container">
+        <div className="row center-xs middle-xs">
+          <div className="col-xs">
+            <span>
+              Designed and Developed with <a href="https://github.com/mianuddin/day-tripping">❤</a> by <a href="http://mianudd.in/">Mian Uddin</a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 );
 
